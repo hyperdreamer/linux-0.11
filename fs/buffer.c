799cc82f7d5a475d4d5720159f90944abafe7372
hyperdreamer <wenhepku@gmail.com>
2023-08-24 20:10:45 +0800

    [PATCH] buffer.c
    My bad! Remove useless locks!

--- fs/buffer.c
+++ fs/buffer.c
@@ -50,33 +50,11 @@ static inline void wait_on_buffer(struct buffer_head* bh)
 	sti();
 }
 
-static inline void lock_buffer(struct buffer_head* bh)
-{
-	cli();
-    while (bh->b_lock) {
-#ifdef DEBUG
-        printkc("buffer.c: lock_inode: Oops!\n");
-#endif
-        sleep_on(&bh->b_wait);
-    } 	
-    bh->b_lock = 1;
-	sti();
-}
-
-static inline void unlock_buffer(struct buffer_head* bh)
-{
-	if (!bh->b_lock) printk("buffer.c: buffer not locked\n");
-    //////////////////////////////////////////////////////////////////////////
-	cli();
-	bh->b_lock = 0;
-	wake_up(&bh->b_wait);
-    sti();
-}
-
 // no check bh validity! be careful!
 static inline void sync_buffer(struct buffer_head* bh)
 {
     wait_on_buffer(bh); // no need to lock strictly
+    //////////////////////////////////////////////////////////////////////////
     if (bh->b_dirt) ll_rw_block(WRITE, bh);
 }
 
@@ -84,19 +62,20 @@ static inline void sync_buffer(struct buffer_head* bh)
 static inline void do_sync(int dev) 
 {
     struct buffer_head* bh = start_buffer;
-    for (register int i = 0; i < NR_BUFFERS; ++i, ++bh) {
+    //;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
+    for (int i = 0; i < NR_BUFFERS; ++i, ++bh) {
         if (bh->b_dev != dev) continue;
         //////////////////////////////////////////////////////////////////////
         wait_on_buffer(bh); // no need to lock strictly
         if (bh->b_dev == dev && bh->b_dirt) ll_rw_block(WRITE, bh);
-        //////////////////////////////////////////////////////////////////////
     }
 }
 
 static inline void invalidate_buffers(int dev)
 {
     struct buffer_head* bh = start_buffer;
-    for (register int i = 0; i < NR_BUFFERS; ++i, ++bh) {
+    //;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
+    for (int i = 0; i < NR_BUFFERS; ++i, ++bh) {
         if (bh->b_dev != dev) continue;
         wait_on_buffer(bh);
         if (bh->b_dev == dev) bh->b_uptodate = bh->b_dirt = 0;
@@ -106,69 +85,71 @@ static inline void invalidate_buffers(int dev)
 #define _hashfn(dev,block) (((unsigned)(dev^block))%NR_HASH)
 #define hash(dev,block) hash_table[_hashfn(dev,block)]
 
-static inline void remove_from_queues(struct buffer_head * bh)
+static inline void remove_from_queues(struct buffer_head* bh)
 {
-/* remove from hash-queue */
-	if (bh->b_next)
-		bh->b_next->b_prev = bh->b_prev;
-	if (bh->b_prev)
-		bh->b_prev->b_next = bh->b_next;
-	if (hash(bh->b_dev,bh->b_blocknr) == bh)
-		hash(bh->b_dev,bh->b_blocknr) = bh->b_next;
-/* remove from free list */
+    /* remove from hash-queue */
+	if (bh->b_next) bh->b_next->b_prev = bh->b_prev;
+	if (bh->b_prev) bh->b_prev->b_next = bh->b_next;
+    /***************************************************************/
+	if (hash(bh->b_dev, bh->b_blocknr) == bh)
+		hash(bh->b_dev, bh->b_blocknr) = bh->b_next;
+    //////////////////////////////////////////////////////////////////////////
+    /* remove from free list */
 	if (!(bh->b_prev_free) || !(bh->b_next_free))
 		panic("Free block list corrupted");
+    /***************************************************************/
 	bh->b_prev_free->b_next_free = bh->b_next_free;
 	bh->b_next_free->b_prev_free = bh->b_prev_free;
-	if (free_list == bh)
-		free_list = bh->b_next_free;
+	if (free_list == bh) free_list = bh->b_next_free;
 }
 
-static inline void insert_into_queues(struct buffer_head * bh)
+static inline void insert_into_queues(struct buffer_head* bh)
 {
-/* put at end of free list */
+    /* put at end of free list */
 	bh->b_next_free = free_list;
 	bh->b_prev_free = free_list->b_prev_free;
+    /***************************************************************/
 	free_list->b_prev_free->b_next_free = bh;
 	free_list->b_prev_free = bh;
-/* put the buffer in new hash-queue if it has a device */
+    //////////////////////////////////////////////////////////////////////////
+    /* put the buffer in new hash-queue if it has a device */
 	bh->b_prev = NULL;
 	bh->b_next = NULL;
-	if (!bh->b_dev)
-		return;
-	bh->b_next = hash(bh->b_dev,bh->b_blocknr);
-	hash(bh->b_dev,bh->b_blocknr) = bh;
+	if (!bh->b_dev) return;
+    /***************************************************************/
+	bh->b_next = hash(bh->b_dev, bh->b_blocknr);
+	hash(bh->b_dev, bh->b_blocknr) = bh;
 	bh->b_next->b_prev = bh;
 }
 
-// No lock! The return value is unreliable!
 static inline struct buffer_head* find_buffer(int dev, int block)
 {		
     struct buffer_head* tmp = hash(dev, block);
+    //;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
     while (tmp) {
-        if (tmp->b_dev == dev && tmp->b_blocknr == block)
-            return tmp;
+        if (tmp->b_dev == dev && tmp->b_blocknr == block) return tmp;
+        /*******************************************************/
         tmp = tmp->b_next;
     }
+    //////////////////////////////////////////////////////////////////////////
     return NULL;
 }
 
-// No lock! Use it cautiously!
-static inline struct buffer_head* find_free_buffer_directly()
+static inline struct buffer_head* find_free_buffer()
 {
 #define BADNESS(bh) (((bh)->b_dirt<<1)+(bh)->b_lock)
 
     struct buffer_head* bh = NULL;
     struct buffer_head* tmp = free_list;
+    //;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
     do {
         if (tmp->b_count) continue;
         if (!bh || BADNESS(tmp)<BADNESS(bh)) {
             bh = tmp;
             if (!BADNESS(tmp)) return bh;
-        }
-        /* and repeat until we find something good */
+        } /* and repeat until we find something good */
     } while ((tmp = tmp->b_next_free) != free_list);
-    /***************************************************************/
+    //////////////////////////////////////////////////////////////////////////
     return NULL;
 }
 
@@ -188,11 +169,10 @@ static inline struct buffer_head* find_free_buffer_directly()
 
 void buffer_init(laddr_t buffer_end)
 {
-    struct buffer_head * h = start_buffer;
-    void* b;
-    //////////////////////////////////////////////////////////////////////////
-    b = (buffer_end == 1<<20) ? (void*) (640*1024) : (void*) buffer_end;
-    while ( (b -= BLOCK_SIZE) >= ((void *) (h+1)) ) {
+    struct buffer_head* h = start_buffer;
+    void* b =(void*) ((buffer_end == 1<<20) ? (640*1024) : buffer_end);
+    //;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
+    while ((b -= BLOCK_SIZE) >= ((void*) (h+1))) {
         h->b_dev = 0;
         h->b_dirt = 0;
         h->b_count = 0;
@@ -206,9 +186,11 @@ void buffer_init(laddr_t buffer_end)
         h->b_next_free = h+1;
         ++h;
         ++NR_BUFFERS;
+        /********************************************************/
         // check the boot-up to check why 0xA0000
-        if (b == (void *) 0x100000) b = (void *) 0xA0000;
+        if (b == (void*) 0x100000) b = (void*) 0xA0000;
     }
+    /***************************************************************/
     --h;
     free_list = start_buffer;
     free_list->b_prev_free = h;
@@ -218,7 +200,7 @@ void buffer_init(laddr_t buffer_end)
      * for (register int i = 0;i < NR_HASH; ++i) hash_table[i]=NULL;
      */
 #ifdef DEBUG
-    for (register int i = 0;i < NR_HASH; ++i) 
+    for (int i = 0;i < NR_HASH; ++i) 
         if (hash_table[i]) { 
             printkc("buffer_init: hash_table is not initialized properly!\n");
             panic("buffer_init: hash_table is not initialized properly!");
@@ -237,16 +219,13 @@ struct buffer_head* get_hash_table(int dev, int block)
 {
     for (;;) {
         struct buffer_head* bh = find_buffer(dev, block);
+        //;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
         if (!bh) return NULL;
         //////////////////////////////////////////////////////////////////////
-        // make sure the return value is reliable, since it cannot be freed
-        // anymore
-        bh->b_count++;
-        //////////////////////////////////////////////////////////////////////
+        bh->b_count++;  // make sure it won't be freed
         wait_on_buffer(bh);
+        /***************************************************************/
         if (bh->b_dev == dev && bh->b_blocknr == block) return bh;
-        // even the bh is valid after wait, the result is still
-        // unreliable, since an interrupt might happen
         //////////////////////////////////////////////////////////////////////
         bh->b_count--;  // make sure it can be freed again
     }
@@ -265,6 +244,7 @@ struct buffer_head* getblk(int dev, int block)
 repeat:
     /* Step 1. Check the hash table */
     struct buffer_head* bh = get_hash_table(dev, block); // reliable
+    //;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
     if (bh) return bh;  // if in hash table directly return it! reliable
     //////////////////////////////////////////////////////////////////////////
     /* 
@@ -272,7 +252,7 @@ repeat:
      * if the block is not in the buffer, you have to find a free 
      * buffer block to read into.
      */
-    bh = find_free_buffer_directly();    // unreliable
+    bh = find_free_buffer();    // unreliable
     /***************************************************************/
     // if no free buffer exists, have to wait for one.
     if (!bh) {
@@ -288,38 +268,29 @@ repeat:
         // write_inode()
         sync_buffer(bh);
         wait_on_buffer(bh);
+        if(bh->b_count) goto repeat;
     }
-    //////////////////////////////////////////////////////////////////////////
-    lock_buffer(bh);
-    if (bh->b_count || bh->b_dirt) {  // lock & check to ensure it is reliable
-#ifdef DEBUG
-        printkc("\ngetblk: The empty buffer block was taken "
-                "while sleeping!\n");
-#endif
-        unlock_buffer(bh);
-        goto repeat;
-    }
-    /***************************************************************/
+    /*#######################################################################*/
+    /*#######################################################################*/
     /* OK, FINALLY we know that this buffer is the only one of it's kind, */
     /* NOTE!! While we slept waiting for this block, somebody else might */
     /* already have added "this" block to the cache. check it */
     if (find_buffer(dev, block)) {
 #ifdef DEBUG
-        printkc("\ngetblk: Someone already inserted a buffer block "
+        printkc("\ngetblk: Someone already loaded a buffer block "
                 "(%d, %d) while sleeping!\n", dev, block);
 #endif
-        unlock_buffer(bh);
         goto repeat;
     }
+    //////////////////////////////////////////////////////////////////////////
+    remove_from_queues(bh);
     /***************************************************************/
+    bh->b_dev = dev;
+    bh->b_blocknr = block;
     bh->b_count = 1;
     bh->b_dirt = 0;
     bh->b_uptodate = 0;
-    remove_from_queues(bh);
-    bh->b_dev = dev;
-    bh->b_blocknr = block;
     insert_into_queues(bh);
-    unlock_buffer(bh);
     //////////////////////////////////////////////////////////////////////////
     return bh;
 }
@@ -329,11 +300,10 @@ void brelse(struct buffer_head* buf)
 	if (!buf) return;
     //////////////////////////////////////////////////////////////////////////
 	wait_on_buffer(buf);
+    /***************************************************************/
 	if (!(buf->b_count--)) panic("Trying to free free buffer");
-    // if buf->b_count ==0 finally, we have to wake up all processes
-    // waiting on this buffer
+    /***************************************************************/
 	wake_up(&buffer_wait);
-    //////////////////////////////////////////////////////////////////////////
 }
 
 /*
@@ -343,6 +313,7 @@ void brelse(struct buffer_head* buf)
 struct buffer_head* bread(int dev, int block)
 {
 	struct buffer_head* bh = getblk(dev, block);
+    //;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
 	if (!bh) panic("bread: getblk returned NULL\n");
     //////////////////////////////////////////////////////////////////////////
     /* 
@@ -351,16 +322,13 @@ struct buffer_head* bread(int dev, int block)
      */
 	if (bh->b_uptodate) return bh;  // no need to read
     //////////////////////////////////////////////////////////////////////////
-
-    //////////////////////////////////////////////////////////////////////////
     /* Step 2. Read it from the device */
     // the up-to-date flag is set by device driver, if read succeeds
     // Check the end_request()
 	ll_rw_block(READ, bh);
+    /***************************************************************/
 	wait_on_buffer(bh);
 	if (bh->b_uptodate) return bh;
-    //////////////////////////////////////////////////////////////////////////
-    
     //////////////////////////////////////////////////////////////////////////
     /* 
      * Step 3. if I/O error happends, release the buffer 
@@ -379,21 +347,18 @@ struct buffer_head* breada(int dev, int first, ...)
 {
     va_list args;
     va_start(args, first);
-    //////////////////////////////////////////////////////////////////////////
+    //;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
     struct buffer_head* bh = getblk(dev, first);
     if (!bh) panic("bread: getblk returned NULL\n");
-    //////////////////////////////////////////////////////////////////////////
     if (!bh->b_uptodate) ll_rw_block(READ, bh);
-    //////////////////////////////////////////////////////////////////////////
+    //;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
     while ((first = va_arg(args, int)) >= 0) {
         struct buffer_head* tmp = getblk(dev, first);
         if (tmp) {
             if (!tmp->b_uptodate) ll_rw_block(READA, tmp);
-            //////////////////////////////////////////////////////////////////
-            --tmp->b_count;     // reada: read ahead but not referenced
+            tmp->b_count--;     // reada: read ahead but not referenced
         }
     }
-    //////////////////////////////////////////////////////////////////////////
     va_end(args);
     //////////////////////////////////////////////////////////////////////////
     wait_on_buffer(bh);
@@ -412,13 +377,12 @@ struct buffer_head* breada(int dev, int first, ...)
 void bread_page(laddr_t address, int dev, int b[4])
 {
     struct buffer_head* bh[4];
-    register int i;
-    //////////////////////////////////////////////////////////////////////////
+    int i;
+    //;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
     for (i = 0; i < 4; ++i)
         if (b[i]) {
-            bh[i] = getblk(dev,b[i]);
-            if (bh[i])
-                if (!bh[i]->b_uptodate) ll_rw_block(READ, bh[i]);
+            bh[i] = getblk(dev, b[i]);
+            if (bh[i] && !bh[i]->b_uptodate) ll_rw_block(READ, bh[i]);
         } 
         else
             bh[i] = NULL;
@@ -435,12 +399,10 @@ void bread_page(laddr_t address, int dev, int b[4])
 // no check device validity! Be careful
 int sync_dev(int dev)
 {
-    //////////////////////////////////////////////////////////////////////////
     //Guess: the reason do_sync() twice is to free buffs for sync_inodes() 
     do_sync(dev); 
     sync_inodes();
     do_sync(dev);
-    //////////////////////////////////////////////////////////////////////////
     return 0;
 }
 
@@ -450,7 +412,7 @@ int sys_sync(void)
 	sync_inodes();		/* write out inodes into buffers */
     //////////////////////////////////////////////////////////////////////////
 	struct buffer_head* bh = start_buffer;
-	for (register int i = 0; i < NR_BUFFERS; ++i, ++bh) {
+	for (int i = 0; i < NR_BUFFERS; ++i, ++bh) {
 		wait_on_buffer(bh);
 		if (bh->b_dirt) ll_rw_block(WRITE, bh);
 	}
