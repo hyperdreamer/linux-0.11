d6f419020556073bca4fcc250f3d8770cd615b61
hyperdreamer <wenhepku@gmail.com>
2023-08-27 16:43:11 +0800

    [MOD] mm.h: Add two macros:
    PAGE_OFFSET_MASK, PAGE_MASK

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #define PAGE_SIZE 4096
+#define PAGE_OFFSET_MASK (PAGE_SIZE-1)
+#define PAGE_MASK (~(PAGE_SIZE-1))
 
 extern unsigned long get_free_page(void);
 extern unsigned long put_page(unsigned long page, unsigned long address);
